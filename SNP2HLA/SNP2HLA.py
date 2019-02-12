1a7ece1e63feeb82eeffae28830d8f91005e523f
Wanson Choi <wschoi.bhlab@gmail.com>
2019-02-12 12:07:37 +0900

    (Code: SNP2HLA.py) "FLIP" code block done.

--- SNP2HLA/SNP2HLA.py
+++ SNP2HLA/SNP2HLA.py
@@ -2,6 +2,7 @@
 
 import os, sys, re
 import argparse, textwrap
+from platform import platform
 
 
 
@@ -40,7 +41,7 @@ def SNP2HLA(_input, _reference_panel, _out,
 
     ### Check dependencies.
 
-    _plink = os.path.join(_dependency, "plink")         # Plink(v1.9)
+    _plink = os.path.join(_dependency, "plink_mac" if not bool(re.search(pattern="Linux", string=platform())) else "plink") #plink v1.9
     _beagle = os.path.join(_dependency, "beagle.jar")   # Beagle(v4.1)
     _linkage2beagle = os.path.join(_dependency, "linkage2beagle.jar")
     _beagle2linkage = os.path.join(_dependency, "beagle2linkage.jar")
@@ -52,7 +53,7 @@ def SNP2HLA(_input, _reference_panel, _out,
         print(std_ERROR_MAIN_PROCESS_NAME + "Please prepare PLINK(v1.90) in 'dependency/' folder.")
         sys.exit()
     if not os.path.exists(_beagle):
-        print(std_ERROR_MAIN_PROCESS_NAME + "Please prepare Beagle(v.4.1) in 'dependency/' folder.")
+        print(std_ERROR_MAIN_PROCESS_NAME + "Please prepare Beagle(v4.1) in 'dependency/' folder.")
         sys.exit()
     if not os.path.exists(_linkage2beagle):
         print(std_ERROR_MAIN_PROCESS_NAME + "Please prepare 'linkage2beagle.jar' in 'dependency/' folder.")
@@ -92,11 +93,15 @@ def SNP2HLA(_input, _reference_panel, _out,
     LINKAGE2BEAGLE = ' '.join(["java", "-Djava.io.tmpdir="+JAVATMP, "-Xmx"+_mem, "-jar", _linkage2beagle])
     BEAGLE2LINKAGE = ' '.join(["java", "-Djava.io.tmpdir="+JAVATMP, "-Xmx"+_mem, "-jar", _beagle2linkage])
 
+    # MERGE = ' '.join(["perl", _merge_table])
+    MERGE = _merge_table
+    PARSEDOSAGE = _parse_dosage
+
 
 
     ### Control Flags
     EXTRACT_MHC = 1
-    FLIP = 0
+    FLIP = 1
     CONVERT_IN = 0
     IMPUTE = 0
     CONVERT_OUT = 0
@@ -116,11 +121,169 @@ def SNP2HLA(_input, _reference_panel, _out,
 
         print("[{}] Extracting SNPs from the MHC.".format(index)); index += 1
 
+        command = ' '.join([PLINK, "--bfile", _input, "--chr 6", "--from-mb 29 --to-mb 34", "--maf 0.025", "--make-bed", "--out", __MHC__])
+        print(command)
+        os.system(command)
 
 
     if FLIP:
 
         print("[{}] Performing SNP quality control.".format(index)); index += 1
+
+        ### Identifying non-A/T non-C/G SNPs to flip
+        command = ' '.join(["echo", "SNP 	POS	A1	A2", ">", OUTPUT+".tmp1"])
+        print(command)
+        os.system(command)
+        command = ' '.join(["cut", "-f2,4-", __MHC__+".bim", ">>", OUTPUT+".tmp1"])
+        print(command)
+        os.system(command)
+
+        command = ' '.join(["echo", "SNP    POSR    A1R A2R", ">", OUTPUT+".tmp2"])
+        print(command)
+        os.system(command)
+        command = ' '.join(["cut", "-f2,4-", _reference_panel+".bim", ">>", OUTPUT+".tmp2"])
+        print(command)
+        os.system(command)
+
+        command = ' '.join([MERGE, OUTPUT+".tmp2", OUTPUT+".tmp1", "SNP", "|", "grep -v -w NA", ">", OUTPUT+".SNPS.alleles"])
+        print(command)
+        os.system(command)
+
+
+
+        ### < Major flip 1 > ###
+
+        command = ' '.join(["awk", "'{if ($3 != $6 && $3 != $7){print $1}}'", OUTPUT+".SNPS.alleles", ">", OUTPUT+".SNPS.toflip1"])
+        print(command)
+        os.system(command)
+
+        command = ' '.join([PLINK, "--bfile", __MHC__, "--flip", OUTPUT+".SNPS.toflip1", "--make-bed", "--out", __MHC__+".FLP"])
+        print(command)
+        os.system(command)
+
+        ## Calculating allele freqeuncy
+        command = ' '.join([PLINK, "--bfile", __MHC__+".FLP", "--freq", "--out", __MHC__+".FLP.FRQ"])
+        print(command)
+        os.system(command)
+
+
+        command = ' '.join(["sed 's/A1/A1I/g'", __MHC__+".FLP.FRQ.frq", "|", "sed 's/A2/A2I/g'", "|", "sed 's/MAF/MAF_I/g'", ">", OUTPUT+".tmp"])
+        print(command)
+        os.system(command)
+
+
+
+        command = ' '.join(["mv", OUTPUT+".tmp", __MHC__+".FLP.FRQ"])
+        print(command)
+        os.system(command)
+
+        command = ' '.join([MERGE, _reference_panel+".FRQ.frq", __MHC__+".FLP.FRQ.frq", "SNP", "|", "grep -v -w NA", ">", OUTPUT+".SNPS.frq"])
+        print(command)
+        os.system(command)
+
+
+
+
+        ### < Major flip 2 > ### (*.parsed file)
+        command = ' '.join(["sed 's/ /\t/g'", OUTPUT+".SNPS.frq", "|",
+                            'awk \'{if ($3 != $8){print $2 "\t" $3 "\t" $4 "\t" $5 "\t" $9 "\t" $8 "\t" 1-$10 "\t*"}else{print $2 "\t" $3 "\t" $4 "\t" $5 "\t" $8 "\t" $9 "\t" $10 "\t."}}\'',
+                            ">", OUTPUT+".SNPS.frq.parsed"])
+        print(command)
+        os.system(command)
+
+
+
+        ### < Major flip 3 > ###
+        # Finding A/T and C/G SNPs
+        command = ' '.join(['awk \'{if (($2 == "A" && $3 == "T") || ($2 == "T" && $3 == "A") || ($2 == "C" && $3 == "G") || ($2 == "G" && $3 == "C")){if ($4 > $7){diff=$4 - $7; if ($4 > 1-$7){corrected=$4-(1-$7)}else{corrected=(1-$7)-$4}}else{diff=$7-$4;if($7 > (1-$4)){corrected=$7-(1-$4)}else{corrected=(1-$4)-$7}};print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6 "\t" $7 "\t" $8 "\t" diff "\t" corrected}}\'',
+                            OUTPUT+".SNPS.frq.parsed", ">", OUTPUT+".SNPS.ATCG.frq"])
+        print(command)
+        os.system(command)
+
+
+
+        ### < Major flip 4 > ###
+
+        # Identifying A/T and C/G SNPs to flip or remove
+        command = ' '.join(["awk '{if ($10 < $9 && $10 < .15){print $1}}'", OUTPUT+".SNPS.ATCG.frq", ">", OUTPUT+".SNPS.toflip2"])
+        print(command)
+        os.system(command)
+
+        command = ' '.join(["awk '{if ($4 > 0.4){print $1}}'", OUTPUT+".SNPS.ATCG.frq", ">", OUTPUT+".SNPS.toremove"])
+        print(command)
+        os.system(command)
+
+
+        ## Identifying non A/T and non C/G SNPs to remove
+        command = ' '.join(['awk \'{if (!(($2 == "A" && $3 == "T") || ($2 == "T" && $3 == "A") || ($2 == "C" && $3 == "G") || ($2 == "G" && $3 == "C"))){if ($4 > $7){diff=$4 - $7;}else{diff=$7-$4}; if (diff > \'%f\'){print $1}}}\''%(_tolerated_diff),
+                            OUTPUT+".SNPS.frq.parsed", ">>", OUTPUT+".SNPS.toremove"])
+        print(command)
+        os.system(command)
+
+
+        command = ' '.join(['awk \'{if (($2 != "A" && $2 != "C" && $2 != "G" && $2 != "T") || ($3 != "A" && $3 != "C" && $3 != "G" && $3 != "T")){print $1}}\'',
+                            OUTPUT+".SNPS.frq.parsed", ">>", OUTPUT+".SNPS.toremove"])
+        print(command)
+        os.system(command)
+
+        command = ' '.join(['awk \'{if (($2 == $5 && $3 != $6) || ($3 == $6 && $2 != $5)){print $1}}\'',
+                            OUTPUT+".SNPS.frq.parsed", ">>", OUTPUT+".SNPS.toremove"])
+        print(command)
+        os.system(command)
+
+
+        ## Making QCd SNP file
+        command = ' '.join([PLINK, "--bfile", __MHC__+".FLP", "--geno 0.2", "--exclude", OUTPUT+".SNPS.toremove", "--flip", OUTPUT+".SNPS.toflip2", "--make-bed", "--out", __MHC__+".QC"])
+        print(command)
+        os.system(command)
+
+        command = ' '.join([PLINK, "--bfile", __MHC__+".QC", "--freq", "--out", __MHC__+".QC.FRQ"])
+        print(command)
+        os.system(command)
+
+        command = ' '.join(["sed 's/A1/A1I/g'", __MHC__+".QC.FRQ.frq", "|", "sed 's/A2/A2I/g'", "|", "sed 's/MAF/MAF_I/g'", ">", OUTPUT+".tmp"])
+        print(command)
+        os.system(command)
+
+        command = ' '.join(["mv", OUTPUT+".tmp", __MHC__+".QC.FRQ.frq"])
+        print(command)
+        os.system(command)
+
+        command = ' '.join([MERGE, _reference_panel+".FRQ.frq", __MHC__+".QC.FRQ.frq", "SNP", "|", "grep -v -w NA", ">", OUTPUT+".SNPS.QC.frq"])
+        print(command)
+        os.system(command)
+
+
+        command = ' '.join(["cut -f2", OUTPUT+".SNPS.QC.frq", "|", "awk '{if (NR > 1){print $1}}'", ">", OUTPUT+".SNPS.toinclude"])
+        print(command)
+        os.system(command)
+
+        command = ' '.join(['echo "SNP     POS    A1    A2"', ">", OUTPUT+".tmp1"])
+        print(command)
+        os.system(command)
+
+        command = ' '.join(["cut -f2,4-", __MHC__+".QC.bim", ">>", OUTPUT+".tmp1"])
+        print(command)
+        os.system(command)
+
+        command = ' '.join([MERGE, OUTPUT+".tmp2", OUTPUT+".tmp1", "SNP", "|", 'awk \'{if (NR > 1){if ($5 != "NA"){pos=$5}else{pos=$2}; print "6\t" $1 "\t0\t" pos "\t" $3 "\t" $4}}\'',
+                            ">", __MHC__+".QC.bim"])
+        print(command)
+        os.system(command)
+
+
+
+        ## Recoding QC'd file as ped
+
+        # temporary ponstponed.
+
+
+        # ## Remove temporary files.
+        # os.system(' '.join(["rm ", OUTPUT+".tmp{1,2}"]))
+        # os.system(' '.join(["rm ", __MHC__+".FLP.*"]))
+        # os.system(' '.join(["rm ", __MHC__+".QC.{ped,map}"]))
+        # os.system(' '.join(["rm ", OUTPUT+".SNPS.*"]))
+
 
 
     if CONVERT_IN:
@@ -193,8 +356,14 @@ if __name__ == "__main__" :
 
     parser.add_argument("-h", "--help", help="\nShow this help message and exit\n\n", action='help')
 
-    parser.add_argument("-i", help="\nInput Plink data file prefix(.bed/.bim/.fam)\n\n", required=True)
-    parser.add_argument("-o", help="\nOutput file prefix\n\n", required=True)
+    parser.add_argument("--input", "-i", help="\nInput Plink data file prefix(.bed/.bim/.fam)\n\n", required=True)
+    parser.add_argument("--out", "-o", help="\nOutput file prefix\n\n", required=True)
+    parser.add_argument("--reference", "-rf", help="\nThe file prefix of reference panel for imputation.\n\n", required=True)
+
+    parser.add_argument("--java-mem", "-mem", help="\nJava memory allocation(ex. '2000m', '2g', or '2G').\n\n", default='2000m')
+    parser.add_argument("--marker-window", help="\nMarker window size for imputation (default: 1000).\n\n", default=1000)
+    parser.add_argument("--tolerated-diff", help="\nTolerated diff (default : 0.15).\n\n", default=0.15)
+    parser.add_argument("--dependency", help="\nPath(folder) to dependecy software.\n\n", default="./") # Default : the folder where SNP2HLA.py is implemented.
 
 
     ##### <for Test> #####
@@ -207,5 +376,5 @@ if __name__ == "__main__" :
 
 
 
-    ##### Additional Argument processing
-
+    SNP2HLA(args.input, args.reference, args.out, _mem=args.java_mem, _marker_window_size=args.marker_window,
+            _tolerated_diff=args.tolerated_diff, _dependency=args.dependency)
